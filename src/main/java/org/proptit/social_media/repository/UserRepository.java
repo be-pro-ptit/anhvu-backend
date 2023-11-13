ff6f6b6ff738376b91e2153acb8368a1b29f308f
VuNA <vunaproptit@gmail.com>
2023-11-13 19:06:34 +0700

    Delete user

--- src/main/java/org/proptit/social_media/repository/UserRepository.java
+++ src/main/java/org/proptit/social_media/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package org.proptit.social_media.repository;
 
+import jakarta.transaction.Transactional;
 import org.proptit.social_media.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByUserId(Long userId);
 
