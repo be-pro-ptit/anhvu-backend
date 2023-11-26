42b515bdee53bd10da95c355b2e94ad1547e26d5
VuNA <vunaproptit@gmail.com>
2023-11-26 19:50:38 +0700

    Author

--- src/main/java/org/proptit/social_media/filter/AuthFilter.java
+++ src/main/java/org/proptit/social_media/filter/AuthFilter.java
@@ -1,11 +1,15 @@
 package org.proptit.social_media.filter;
 
 
+import com.google.gson.Gson;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.proptit.social_media.base.BaseResponse;
+import org.proptit.social_media.base.Status;
 import org.proptit.social_media.exeption.NotFoundException;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -13,17 +17,33 @@ import java.io.IOException;
 @Component
 @Order(1)
 public class AuthFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
 
+    private Gson gson;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        gson = new Gson();
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest rq = (HttpServletRequest) servletRequest;
         HttpServletResponse rp = (HttpServletResponse) servletResponse;
-        System.out.println("AuthFilter: " + rq.getRequestURI());
-//        throw new NotFoundException("Not found");
+        String url = rq.getRequestURI();
+        if (url.startsWith("/api/auth")) {
+            filterChain.doFilter(rq, rp);
+            return;
+        } else if (url.startsWith("/api")) {
+            String accessToken = rq.getHeader("Authorization");
+            if (accessToken == null) {
+                rp.setStatus(401);
+                rp.getWriter()
+                  .write(gson.toJson(BaseResponse.error(new Status("unauthorized", "Chưa đăng nhập"))));
+                return;
+            }
+            filterChain.doFilter(rq, rp);
+            return;
+        }
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
