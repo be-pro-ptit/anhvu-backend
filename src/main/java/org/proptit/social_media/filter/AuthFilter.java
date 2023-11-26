ae99911db7ace6104268de55c5fd14fe9c8611b1
VuNA <vunaproptit@gmail.com>
2023-11-26 19:58:30 +0700

    Check valid token

--- src/main/java/org/proptit/social_media/filter/AuthFilter.java
+++ src/main/java/org/proptit/social_media/filter/AuthFilter.java
@@ -2,14 +2,14 @@ package org.proptit.social_media.filter;
 
 
 import com.google.gson.Gson;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.proptit.social_media.base.BaseResponse;
 import org.proptit.social_media.base.Status;
-import org.proptit.social_media.exeption.NotFoundException;
+import org.proptit.social_media.service.jwt.JwtService;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -19,6 +19,11 @@ import java.io.IOException;
 public class AuthFilter implements Filter {
 
     private Gson gson;
+    private final JwtService jwtService;
+
+    public AuthFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
 
     @Override
     public void init(FilterConfig filterConfig) {
@@ -35,11 +40,28 @@ public class AuthFilter implements Filter {
             return;
         } else if (url.startsWith("/api")) {
             String accessToken = rq.getHeader("Authorization");
-            if (accessToken == null) {
+            if (accessToken == null || !accessToken.startsWith("Bearer")) {
                 rp.setStatus(401);
                 rp.getWriter()
-                  .write(gson.toJson(BaseResponse.error(new Status("unauthorized", "Chưa đăng nhập"))));
+                  .print(gson.toJson(BaseResponse.error(new Status("unauthorized", "Chưa đăng nhập"))));
+                rp.setHeader("Content-Type", "application/json");
                 return;
+            } else {
+                try {
+                    jwtService.validateAccessToken(accessToken.substring(7));
+                } catch (ExpiredJwtException expiredJwtException) {
+                    rp.setStatus(401);
+                    rp.getWriter()
+                      .print(gson.toJson(BaseResponse.error(new Status("expired", "Token hết hạn"))));
+                    rp.setHeader("Content-Type", "application/json");
+                    return;
+                } catch (Exception e) {
+                    rp.setStatus(401);
+                    rp.getWriter()
+                      .print(gson.toJson(BaseResponse.error(new Status("invalid", "Token không hợp lệ"))));
+                    rp.setHeader("Content-Type", "application/json");
+                    return;
+                }
             }
             filterChain.doFilter(rq, rp);
             return;
